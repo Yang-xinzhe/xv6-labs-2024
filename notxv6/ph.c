c2fff5a27fa389972e73a339fe628a24dea824b9
Yangxinzhe <1245314699@qq.com>
2025-05-24 17:18:07 +0800

    fix: ph_fast failed due to all threads share unique mutex lock

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,7 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
-pthread_mutex_t lock;
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -48,7 +48,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
-  pthread_mutex_lock(&lock);
+  pthread_mutex_lock(&lock[i]);
   if(e){
     // update the existing key.
     e->value = value;
@@ -56,7 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-  pthread_mutex_unlock(&lock);
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -66,11 +66,11 @@ get(int key)
 
 
   struct entry *e = 0;
-  pthread_mutex_lock(&lock);
+  pthread_mutex_lock(&lock[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-  pthread_mutex_unlock(&lock);
+  pthread_mutex_unlock(&lock[i]);
   return e;
 }
 
@@ -114,7 +114,10 @@ main(int argc, char *argv[])
     exit(-1);
   }
 
-  pthread_mutex_init(&lock, NULL);
+  for(int i = 0 ; i < NBUCKET ; ++i){
+    pthread_mutex_init(&lock[i], NULL);
+  }
+  
 
   nthread = atoi(argv[1]);
   tha = malloc(sizeof(pthread_t) * nthread);
